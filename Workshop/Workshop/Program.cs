141a2eddfc4fffb307af3147deaf67bae0eb73ef
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-14 11:37:18 +0530

    [Sanju] ADD : Starting with zero

--- Workshop/Workshop/Program.cs
+++ Workshop/Workshop/Program.cs
@@ -2,11 +2,16 @@
 
 namespace Workshop
 {
-    class Program
+
+    public class Program
     {
+        int START_POISTION = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("=====Welcome to Snake And Ladder=====");
+
+            Program pg = new Program();
+            Console.WriteLine("The start postion of game is : " + pg.START_POISTION);
         }
     }
 }
