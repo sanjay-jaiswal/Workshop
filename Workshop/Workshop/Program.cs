a4e4e7a6ceefbf75c018d0c7441bf7e8e9a5856c
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-14 12:18:04 +0530

    [Sanju] ADD : Roll the die to get position from 0 to 6

--- Workshop/Workshop/Program.cs
+++ Workshop/Workshop/Program.cs
@@ -2,16 +2,26 @@
 
 namespace Workshop
 {
-
-    public class Program
+    class Program
     {
-        int START_POISTION = 0;
+        const int START_POISTION = 0;
+        public static int currentPosition = 0;
+
+        public static int rollTheDie()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 7);
+            return num;
+        }
+
         static void Main(string[] args)
         {
+            currentPosition += rollTheDie();
             Console.WriteLine("=====Welcome to Snake And Ladder=====");
+            Console.WriteLine("The start postion of game is : " + START_POISTION);
+           // Console.WriteLine("The end postion of game is : " + END_POSTION);
 
-            Program pg = new Program();
-            Console.WriteLine("The start postion of game is : " + pg.START_POISTION);
+            Console.ReadLine();
         }
     }
 }
