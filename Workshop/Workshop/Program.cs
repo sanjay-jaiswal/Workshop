1d7d551adf51eddea309eaac7bced279c1df4dbe
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-14 11:28:08 +0530

    [Sanju] ADD : Welcome message

---
+++ Workshop/Workshop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====Welcome to Snake And Ladder=====");
+        }
+    }
+}
